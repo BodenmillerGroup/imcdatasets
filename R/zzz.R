2ea5d2224559d350ec9fe3c4bea898c995e1eb98
Nicolas Damond <nicolas.damond@gmail.com>
2022-12-07 15:13:47 +0000

    update documentation HochSchulz

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    fl <- system.file("extdata", "metadata.csv", package = "imcdatasets")
-    titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-    ExperimentHub::createHubAccessors(pkgname, titles)
-}
